411a45a7fefd6a54e09da8e057ffb343e4a5f94d
Ivan-Teplyakov <42812703+Ivan-Teplyakov@users.noreply.github.com>
2023-07-04 21:30:29 +0300

    Update Variant 2.py

--- Code/Variant 2.py
+++ Code/Variant 2.py
@@ -1,15 +1,15 @@
 # Credential check program
 # Variant 2: "While loop"
 
-stored_login = "sergiy"
+stored_username = "sergiy"
 stored_password = "123456"
 attempts = 0
 
 while attempts < 5:
 
-    login = input('Enter your username and press "Enter": ')
+    username = input('Enter your username and press "Enter": ')
     password = input('Enter your password and press "Enter": ')
-    if login == stored_login and password == stored_password:
+    if username == stored_username and password == stored_password:
 
         print('Welcome to Tax Management System!')
         break
