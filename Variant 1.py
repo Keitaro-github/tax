46bc7b76a9b74383e05c1236685580be98860541
Keitaro-github <138602879+Keitaro-github@users.noreply.github.com>
2023-07-10 21:10:49 +0200

    Add files via upload

---
+++ Variant 1.py
@@ -0,0 +1,46 @@
+# Credentials check program
+# Variant 1: "No loops"
+
+stored_username = "sergiy"
+stored_password = "123456"
+username = input('Enter your username and press "Enter": ')
+password = input('Enter your password and press "Enter": ')
+
+if username != stored_username or password != stored_password:
+    print('Incorrect credentials, please check your input!')
+    username = input('Enter your username and press "Enter": ')
+    password = input('Enter your password and press "Enter": ')
+
+    if username != stored_username or password != stored_password:
+        print('Incorrect credentials, please check your input!')
+        username = input('Enter your username and press "Enter": ')
+        password = input('Enter your password and press "Enter": ')
+
+        if username != stored_username or password != stored_password:
+            print('Incorrect credentials, please check your input!')
+            username = input('Enter your username and press "Enter": ')
+            password = input('Enter your password and press "Enter": ')
+
+            if username != stored_username or password != stored_password:
+                print('Incorrect credentials, please check your input!')
+                username = input('Enter your username and press "Enter": ')
+                password = input('Enter your password and press "Enter": ')
+
+                if username != stored_username or password != stored_password:
+                    print('Incorrect credentials, please check your input!')
+                    print('Oops, too many wrong attempts, please contact administrator!')
+
+                else:
+                    print('Welcome to Tax Management System!')
+
+            else:
+                print('Welcome to Tax Management System!')
+
+        else:
+            print('Welcome to Tax Management System!')
+
+    else:
+        print('Welcome to Tax Management System!')
+
+else:
+    print('Welcome to Tax Management System!')
