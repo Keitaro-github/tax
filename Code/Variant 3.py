26c811d2cb3ad7ecab965b9d3a0c2e0b3ff7930a
Ivan-Teplyakov <42812703+Ivan-Teplyakov@users.noreply.github.com>
2023-07-20 21:46:56 +0300

    Update Variant 3.py

--- Code/Variant 3.py
+++ Code/Variant 3.py
@@ -4,9 +4,8 @@
 stored_username = "sergiy"
 stored_password = "123456"
 attempts = 0
-attempts_limit = 5
 
-
+"""
 for _ in range(5):
     username = input('Enter your username and press "Enter": ')
     password = input('Enter your password and press "Enter": ')
@@ -22,3 +21,20 @@ for _ in range(5):
     elif attempts == 5:
         print('Oops, too many wrong attempts, please contact administrator!')
         quit()
+"""
+
+result = False
+
+for _ in range(5):
+    username = input('Enter your username and press "Enter": ')
+    password = input('Enter your password and press "Enter": ')
+    
+    if username != stored_username or password != stored_password:
+        print('Incorrect credentials, please check your input!')
+    else:
+        print('Welcome to Tax Management System!')
+        result = True
+        break
+
+if result is False:
+    print ("Oops, too many wrong attempts, please contact administrator!")
