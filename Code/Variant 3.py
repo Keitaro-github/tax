6fd764b721c2c5ae69126c75541c6ae0a344f44a
Keitaro Akihara <sergeybol@hotmail.com>
2023-07-21 19:45:39 +0200

    Add True Boolean and small fixes

--- Code/Variant 3.py
+++ Code/Variant 3.py
@@ -10,17 +10,20 @@ attempts_limit = 5
 def sign_in():
     username = input('Enter your username and press "Enter": ')
     password = input('Enter your password and press "Enter": ')
-    if username != stored_username or password != stored_password:
+    if username == stored_username and password == stored_password:
+        return True
+    elif username != stored_username or password != stored_password:
         return False
 
 
 for _ in range(5):
-    if sign_in() is False:
-        print('Incorrect credentials, please check your input!')
-        attempts = attempts + 1
-    else:
+    if sign_in() is True:
         print('Welcome to Tax Management System!')
         break
+
+    else:
+        print('Incorrect credentials, please check your input!')
+        attempts = attempts + 1
 
 if attempts == attempts_limit:
     print("Oops, too many wrong attempts, please contact administrator!")
