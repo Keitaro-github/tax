a705581435493a08aa2b5e0f860f18b8335c0ec8
Keitaro Akihara <sergeybol@hotmail.com>
2023-08-11 22:27:37 +0200

    Add deletion and printout of user

--- Code/Variant 3.py
+++ Code/Variant 3.py
@@ -1,18 +1,57 @@
 # Credentials check program
 # Variant 3: "Range function"
-
-# stored_username = "sergiy"
-# stored_password = "123456"
 attempts = 0
 attempts_limit = 5
 users = [
-    {"username": "Keitaro", "password": "123456", 'ID': 1},
-    {"username": "Ivan", "password": "654321", 'ID': 2},
-    {"username": "Ben", "password": "999999", 'ID': 3},
-    {"username": "David", "password": "888888", 'ID': 4},
-    {"username": "Chris", "password": "777777", 'ID': 5},
-    {"username": "Gregor", "password": "666666", 'ID': 6}
+    {"username": "Keitaro", "password": "123456", 'user_id': 1},
+    {"username": "Ivan", "password": "654321", 'user_id': 2},
+    {"username": "Ben", "password": "999999", 'user_id': 3},
+    {"username": "David", "password": "888888", 'user_id': 4},
+    {"username": "Chris", "password": "777777", 'user_id': 5},
+    {"username": "Gregor", "password": "666666", 'user_id': 6}
 ]
+
+
+def delete_user_by_id(users):
+    while True:
+        print('Please enter user ID for deletion:')
+        try:
+            user_id_input = int(input())
+        except ValueError:
+            print('Invalid input. Please neter a valid user ID.')
+            continue
+        for user in users:
+            if user['user_id'] == user_id_input:
+                users.remove(user)
+                print(f'User with ID {user_id_input} has been deleted')
+                break
+            else:
+                print(f'User with ID {user_id_input} not found')
+                break
+
+
+delete_user_by_id(users)
+
+
+def user_info_printout(users):
+    while True:
+        print('Please enter user ID for printout:')
+        try:
+            user_id_input = int(input())
+        except ValueError:
+            print('Invalid input. Please neter a valid user ID.')
+            continue
+        found = False
+        for user in users:
+            if user['user_id'] == user_id_input:
+                print(user)
+                found = True
+                break
+        if not found:
+            print(f'User with ID {user_id_input} not found')
+
+
+user_info_printout(users)
 
 
 def show_list():
@@ -25,7 +64,6 @@ def validate_password(password):
     password_length = len(password)
     password_digits_only = str.isdigit(password)
     password_alphabets_only = str.isalpha(password)
-
     if password_length <= 7:
         print('Too short password!')
         return False
@@ -42,26 +80,22 @@ def validate_password(password):
 def add_user():
     show_list()
     while True:
-        print('***Exit registration with empty input')
+        print('')
         username = input('Register your username and press "Enter": ')
-        if username.strip() == '':
-            break
         password = input('Register your password and press "Enter": ')
-        if password.strip() == '':
-            break
-        if validate_password(password) is False:
-            continue
+        while not validate_password(password):
+            password = input('Please enter a valid password: ')
         id_num = input('Register your ID and press "Enter": ')
-        if id_num.strip() == '':
-            break
-        users.append({"username": username, "password": password, "ID": id_num})
-    show_list()
+        if users.append({"username": username, "password": password, "ID": id_num}):
+            add_user()
 
 
 add_user()
+show_list()
 
 
 def sign_in():
+    print('')
     username_input = input('Enter your username and press "Enter": ')
     password_input = input('Enter your password and press "Enter": ')
     for user in users:
